9f233886fa1d3541b04522c40452e0c963e8b545
Jermaine Kelly <jerjunkel@gmail.com>
2024-09-10 17:13:58 -0400

    feat(server): add cors

--- api_server/src/index.js
+++ api_server/src/index.js
@@ -1,8 +1,10 @@
 import config from "./config/index.js";
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 const app = new express();
 
+app.use(cors());
 app.use(routes);
 
 app.get("/", (_, res) => {
