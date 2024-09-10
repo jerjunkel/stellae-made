74c702849e949ccffc2fc6926b23dc6740f9678b
Jermaine Kelly <jerjunkel@gmail.com>
2024-09-10 17:54:39 -0400

    feat(server): increase images result

--- api_server/src/services/image.service.js
+++ api_server/src/services/image.service.js
@@ -2,7 +2,7 @@ import apiClient from "../common/apiClient.js";
 
 const findImageByQuery = async (query) => {
   try {
-    const response = await apiClient.get(`/search?query=${query}`);
+    const response = await apiClient.get(`/search?query=${query}&per_page=30`);
     const data = response.data;
     const images = data.photos.map((item) => {
       const { url, src, alt } = item;
