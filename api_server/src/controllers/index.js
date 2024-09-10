4488f83a1811f4cf49551de406a78d5f97c29b6e
Jermaine Kelly <jerjunkel@gmail.com>
2024-09-10 18:30:45 -0400

    feat(server): add server next page

--- api_server/src/controllers/index.js
+++ api_server/src/controllers/index.js
@@ -1,9 +1,11 @@
 import service from "../services/image.service.js";
 const findImageByQuery = async (req, res) => {
   const query = encodeURI(req.query.query);
-
   try {
     const response = await service.findImageByQuery(query);
+    const { next_page } = response;
+    const nextQuery = next_page.split("search/")[1];
+    response.next_page = `http://${req.headers.host}/search/${nextQuery}`;
     return res.status(200).json(response);
   } catch (e) {
     console.log(e);
