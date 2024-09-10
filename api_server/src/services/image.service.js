3d021228f7084a27f3b3fbe8e5345006e8f847e4
Jermaine Kelly <jerjunkel@gmail.com>
2024-09-10 18:09:49 -0400

    refactor(server): add page and next_page to response

--- api_server/src/services/image.service.js
+++ api_server/src/services/image.service.js
@@ -3,8 +3,8 @@ import apiClient from "../common/apiClient.js";
 const findImageByQuery = async (query) => {
   try {
     const response = await apiClient.get(`/search?query=${query}&per_page=30`);
-    const data = response.data;
-    const images = data.photos.map((item) => {
+    const { page, photos, next_page } = response.data;
+    const images = photos.map((item) => {
       const { url, src, alt } = item;
       return {
         url,
@@ -13,7 +13,11 @@ const findImageByQuery = async (query) => {
       };
     });
 
-    return images;
+    return {
+      page,
+      images,
+      next_page,
+    };
   } catch (e) {
     console.log(e);
   }
