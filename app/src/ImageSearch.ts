23badc71d3021a3f4eb54296ce65d68f5924ddb2
Jermaine Kelly <jerjunkel@gmail.com>
2024-09-11 00:02:45 -0400

    refactor(app): add ImageSearch class

---
+++ app/src/ImageSearch.ts
@@ -0,0 +1,23 @@
+import ImageFetcher from "./ImageFetcher.js";
+import ImageStore from "./ImageStore.js";
+import Image from "./Image.js";
+
+export default class ImageSearch {
+  private store = new ImageStore();
+  private fetcher = new ImageFetcher();
+  private baseUrl = "http://localhost:3000/search";
+  constructor() {}
+
+  async findImagesByQuery(query: string): Promise<Image[]> {
+    const response = await this.fetcher.fetchImages(
+      `${this.baseUrl}?query=${query}`
+    );
+    this.store.addImages(response.images);
+    return response.images;
+  }
+
+  async getNextSearchResults(): Promise<Image[]> {
+    const response = await this.fetcher.fetchImages(this.fetcher.nextPageURL);
+    return response.images;
+  }
+}
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,5 +1,67 @@
-import ImageFetcher from "./ImageFetcher.js";
-import ImageStore from "./ImageStore.js";
+import ImageSearch from "./ImageSearch.js";
+import Image from "./Image.js";
+const imageSerach = new ImageSearch();
 
-const fetcher = new ImageFetcher();
-const store = new ImageStore();
+setup();
+
+function setup() {
+  const form = document.querySelector("form");
+  const button = document.querySelector("button");
+  const inputText = document.querySelector("input");
+  const columns = document.querySelectorAll(".column");
+  let results = 0;
+
+  if (!form || !button) return;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  button.addEventListener("click", async (e) => {
+    const searchQuery = inputText?.value ?? "random";
+    columns.forEach((item) => (item.innerHTML = ""));
+    const images = await imageSerach.findImagesByQuery(searchQuery);
+    addImageResults(images);
+    addIntersectionObserver();
+  });
+
+  function addImageResults(images: Image[]) {
+    const numberOfColumns = columns.length;
+    images.forEach((image) => {
+      results += 1;
+      const col = columns[results % numberOfColumns];
+      col.appendChild(createImageElement(image));
+    });
+  }
+
+  async function loadNextPageResults() {
+    try {
+      const images = await imageSerach.getNextSearchResults();
+      addImageResults(images);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function createImageElement(image: Image) {
+    const img = document.createElement("img");
+    img.src = image.src.medium;
+    img.alt = image.alt;
+    return img;
+  }
+
+  function addIntersectionObserver() {
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].intersectionRatio <= 0) return;
+        console.log("Hello!");
+        loadNextPageResults();
+      },
+      {
+        rootMargin: "0px 0px 100px 0px",
+        threshold: 1.0,
+      }
+    );
+    intersectionObserver.observe(document.querySelector(".more")!);
+  }
+}
