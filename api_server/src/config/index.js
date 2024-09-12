96d4fd8d250e818124f32cbe2cf090cdb882ceb7
Jermaine Kelly <jerjunkel@gmail.com>
2024-09-12 10:29:11 -0400

    chore: rename server directory

--- api_server/src/config/index.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-
-export default {
-  API_KEY: process.env.API_KEY,
-  API_ENDPOINT: process.env.API_ENDPOINT,
-  PORT: process.env.PORT || 5000,
-  ENV: process.env.ENVIRONMENT || "development",
-};
--- api_server/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import controller from "../controllers/index.js";
-
-const router = new Router();
-router.get("/search", controller.findImageByQuery);
-
-export default router;
